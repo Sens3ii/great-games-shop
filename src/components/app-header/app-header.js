12c1d8a7c7dff527235163fb29a95f8ad2eccacc
Sens3ii <densteeper@gmail.com>
2021-07-24 20:03:03 +0600

    start project

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,6 @@
+import React from "react";
+const AppHeader = () => {
+	return <header className="header">Home</header>;
+};
+
+export default AppHeader;
---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppHeader from "../app-header";
+
+const App = () => {
+	return (
+		<div className="app">
+			<AppHeader />
+		</div>
+	);
+};
+
+export default App;
